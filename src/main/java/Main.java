998f86332b35e37e2c6caeb2b63832b38ab993d4
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-25 12:43:15 -0400

    finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,36 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter start of first range: ");
         int start1 = scanner.nextInt();
+        System.out.print("Enter end of first range: ");
         int end1 = scanner.nextInt();
 
+        System.out.print("Enter start of second range: ");
         int start2 = scanner.nextInt();
+        System.out.print("Enter end of second range: ");
         int end2 = scanner.nextInt();
+
+        // int start1 = 2;
+        // int end1 = 6;
+
+        // int start2 = 1;
+        // int end2 = 4;
 
         RangeAdder adder1 = new RangeAdder(start1, end1);
         RangeAdder adder2 = new RangeAdder(start2, end2);
 
+        adder1.start();
+        adder2.start();
+
+        adder1.join();
         long partialSum1 = adder1.getSum();
+        adder2.join();
         long partialSum2 = adder2.getSum();
 
-        long sum = partialSum1 + partialSum2; 
+        long sum = partialSum1 + partialSum2;
 
-        System.out.println(sum);
+        System.out.println("The sum of both ranges is: " + sum);
 
         scanner.close();
     }
